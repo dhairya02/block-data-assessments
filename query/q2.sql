fa0d825649b299528c955a2e24066b92c9c7b491
Dhairya Upadhyay <32928312+dhairya02@users.noreply.github.com>
2022-09-02 09:22:27 +0530

    Delete query directory

--- query/q2.sql
+++
@@ -1,6 +0,0 @@
-select extract(month from closed_won_date) as top_grossing_month, sum(deal_value_usd) as gross_sum
-from deal
-where closed_won_date is not null
-group by top_grossing_month
-order by gross_sum desc
-limit 1;
